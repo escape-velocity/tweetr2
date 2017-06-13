7466fb9cf1ae396ed5af4dc1e776fc818e046e15
Bill <networker747@gmail.com>
2017-06-13 21:23:58 +0000

    added in character counter

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,14 @@
+
+$(function(){
+  $('.container .new-tweet textarea').keyup(function(){
+    const inputLength = $(this).val().length;
+      // console.log("Bill", inputLength)
+      const $counter = $('.new-tweet .counter').text(140 - inputLength);
+      // console.log($counter)
+      if(inputLength <= 140) {
+        $counter.css('color', 'black');
+      } else {
+        $counter.css('color', 'red');
+      }
+  });
+});
